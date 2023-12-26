3d9fdfe746946bbd9cfe647cb28aca7ee079d3f3
vakkaskarakurt <karakurtvakkas@gmail.com>
2023-12-26 14:52:29 +0300

    delete

--- worth2watch/Database/comment_db/translate_tr_eng.py
+++
@@ -1,6 +0,0 @@
-from googletrans import Translator
-
-def translate_text(text, source_language='tr', target_language='en'):
-    translator = Translator()
-    result = translator.translate(text, src=source_language, dest=target_language)
-    return result.text
