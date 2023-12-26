8f42146237b2cfd57377727acf0fb8f20afb192a
klavas35 <sleepy19@hotmail.com>
2023-12-26 22:32:18 +0300

    testing failed

--- worth2watch/agent_main/agent_main.py
+++ worth2watch/agent_main/agent_main.py
@@ -1,13 +1,13 @@
-from worth2watch.Database.comment_db.reddit.reddit_api import search_reddit
+# from worth2watch.Database.comment_db.reddit.reddit_api import search_reddit
 from worth2watch.Database.comment_db.youtube.yt_trailer_comments import get_youtube_comments
 from worth2watch.Database.comment_db.comment_requests import movie_names
 def main_agent(movie_name, reddit_status, youtube_status):
-    if reddit_status:
-        reddit_comments = search_reddit(movie_name, comment_limit=100, search_limit=1, thread_depth=1)
-    if youtube_status:
-        youtube_comments = get_youtube_comments(movie_name, 100)
-    return reddit_comments, youtube_comments
-
+    # if reddit_status:
+        # reddit_comments = search_reddit(movie_name, comment_limit=100, search_limit=1, thread_depth=1)
+    # if youtube_status:
+        # youtube_comments = get_youtube_comments(movie_name, 100)
+    # return reddit_comments, youtube_comments
+    return ''
 # print(main_agent("Avengers Infinity War", True, True))
 def agent_movie_caller(is_reddit, is_youtube):
     movie_names_list = movie_names()
