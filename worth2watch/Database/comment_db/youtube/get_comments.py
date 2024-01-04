9dd15ea1d25a70fd599cd43f09b60226034f19fd
Kürşat Edabalı Yıldırım <40580360+kedabaliyildirim@users.noreply.github.com>
2024-01-04 10:19:56 +0300

    Deleted file creation logic as veecel is read only environment

--- worth2watch/Database/comment_db/youtube/get_comments.py
+++ worth2watch/Database/comment_db/youtube/get_comments.py
@@ -1,19 +1,13 @@
 import googleapiclient.discovery
 from requests import HTTPError
-from worth2watch.Database.comment_db.youtube.find_movie import storage_checker
 
 
 def get_comment_text(video_id, api_key, maxResults=100):
     api_service_name = "youtube"
     api_version = "v3"
-    non_error_file = open("non_error.txt", "a")
     try:
         youtube = googleapiclient.discovery.build(
             api_service_name, api_version, developerKey=api_key)
-        storage_check = storage_checker(video_id, path="non_error.txt")
-        if video_id in storage_check:
-            print("Already searched")
-            return []
         request = youtube.commentThreads().list(
             part="snippet",
             videoId=video_id,
